fb303b97e614b305d2c90215c9bc6faa24b95562
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2015-01-12 21:37:01 +0300

    handle case when custom options is nil

--- lib/railroady/diagram_graph.rb
+++ lib/railroady/diagram_graph.rb
@@ -107,7 +107,7 @@ class DiagramGraph
            # Return subgraph format
            return "subgraph cluster_#{name.downcase} {\n\tlabel = #{quote(name)}\n\t#{attributes.join("\n  ")}}"
     end # case
-    options = [options, custom_options].reject{|o| o.empty?}.join(', ')
+    options = [options, custom_options].compact.reject{|o| o.empty?}.join(', ')
     return "\t#{quote(name)} [#{options}]\n"
   end # dot_node
 
