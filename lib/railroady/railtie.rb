fe8c613a3041b96953cce8b3b1dd33689f81bd52
Thomas Nys <nysthee@gmail.com>
2015-04-13 00:18:17 +0200

    Mitigates Style/Tab

--- lib/railroady/railtie.rb
+++ lib/railroady/railtie.rb
@@ -4,9 +4,9 @@ module RailRoady
   class Railtie < Rails::Railtie
 
     rake_tasks do
-	  f = File.join(File.dirname(__FILE__), '..', '..', 'tasks', 'railroady.rake')
+    f = File.join(File.dirname(__FILE__), '..', '..', 'tasks', 'railroady.rake')
      load f
-		# load 'tasks/railroady.rake'
+    # load 'tasks/railroady.rake'
     end
   end
 end
